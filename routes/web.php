a95db527c993665168e5e6c9502ae894c35cf043
Konstantin <levachin@luckandroll.com>
2020-09-13 23:33:36 +0300

    Added admin page

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', 'HomeController@index')->name('home');
 //Route::get('/', function () {
 //    return view('home');
@@ -20,3 +23,15 @@ Route::get('/', 'HomeController@index')->name('home');
 Route::get('/verify/{token}', 'Auth\RegisterController@verify')->name('register.verify');
 
 Route::get('/cabinet', 'Cabinet\HomeController@index')->name('cabinet');
+
+Route::group(
+    [
+        'prefix' => 'admin',
+        'as' => 'admin.',
+        'namespace' => 'Admin',
+        'middleware' => ['auth'],
+    ],
+    function () {
+        Route::get('/', 'HomeController@index')->name('home');
+    }
+);
