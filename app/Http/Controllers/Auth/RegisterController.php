e6a5fb7b803a96ad3514535cb07d0cc8240f4f06
Konstantin <levachin@luckandroll.com>
2020-09-13 23:16:35 +0300

    Added custom requests

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Http\Requests\Auth\RegisterRequest;
 use App\User;
 use App\Mail\Auth\VerifyMail;
 use App\Http\Controllers\Controller;
@@ -41,14 +42,8 @@ class RegisterController extends Controller
     }
 
 
-    public function register(Request $request)
+    public function register(RegisterRequest $request)
     {
-        $this->validate($request, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
-
         $user = User::create([
             'name' => $request['name'],
             'email' => $request['email'],
