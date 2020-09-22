04db8aabdca5115e2b03b063bac97a2540aa940c
Konstantin <levachin@luckandroll.com>
2020-09-23 01:45:34 +0300

    Added user verify command

---
+++ app/Console/Commands/User/VerifyCommand.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Console\Commands\User;
+
+use App\Entity\User;
+use App\UseCases\Auth\RegisterService;
+use Illuminate\Console\Command;
+
+class VerifyCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:verify {email}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Verify user email';
+
+    private $service;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(RegisterService $service)
+    {
+        parent::__construct();
+        $this->service = $service;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $email = $this->argument('email');
+
+        /** @var User $user */
+        if (!$user = User::where('email', $email)->first()) {
+            $this->error('Undefined user with email ' . $email);
+            return false;
+        }
+
+        try {
+            $this->service->verify($user->id);
+        } catch (\DomainException $e) {
+            $this->error($e->getMessage());
+            return false;
+        }
+
+        $this->info('User is successfully verified');
+        return true;
+    }
+}
