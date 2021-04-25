b0fa19f830264662d74aea9749c3e507f72d9c0f
KonstantinLev <x-stels@bk.ru>
2021-04-25 14:02:08 +0300

    Migrated to json resources

--- app/Http/Controllers/Api/User/ProfileController.php
+++ app/Http/Controllers/Api/User/ProfileController.php
@@ -6,6 +6,7 @@ use App\Entity\User\User;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Cabinet\ProfileEditRequest;
 use App\Http\Serializers\UserSerializer;
+use App\Http\Resources\User\ProfileResource;
 use App\UseCases\Profile\ProfileService;
 use Illuminate\Http\Request;
 
@@ -15,29 +16,33 @@ class ProfileController extends Controller
     /**
      * @var UserSerializer
      */
-    private $serializer;
+    //private $serializer;
 
-    public function __construct(ProfileService $service, UserSerializer $serializer)
+    public function __construct(ProfileService $service)
     {
         $this->service = $service;
-        $this->serializer = $serializer;
+        //$this->serializer = $serializer;
     }
 
-    public function show(Request $request): array
+    public function show(Request $request)
     {
         /** @var User $user */
         $user = $request->user();
 
-        return $this->serializer->profile($user);
+        return new ProfileResource($request->user());
+
+        //return $this->serializer->profile($user);
     }
 
-    public function update(ProfileEditRequest $request): array
+    public function update(ProfileEditRequest $request)
     {
         $this->service->edit($request->user()->id, $request);
 
         /** @var User $user */
         $user = User::findOrFail($request->user()->id);
 
-        return $this->serializer->profile($user);
+        return new ProfileResource($user);
+
+        //return $this->serializer->profile($user);
     }
 }
